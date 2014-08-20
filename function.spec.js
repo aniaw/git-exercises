28c6d943120515c4d13c5a4146797b291a7959b5
ania <ania_w@outlook.com>
2014-08-20 09:01:52 +0200

    Add test number string

--- function.spec.js
+++ function.spec.js
@@ -6,5 +6,9 @@ describe('functions test', function () {
         it('should reverse the number', function () {
             expect(answers.reverseNumber(1234)).toEqual(4321);
         });
+         it('should reverse the number from "number string"', function () {
+            expect(answers.reverseNumber('1234')).toBe(4321);
+        });
+
     });
 });
